4123865405575be2853a5682dec4a4eaed154aff
kpla001 <kpla001@fiu.edu>
2021-09-11 12:07:31 -0400

    modify user model to include first and last names

--- models/User.model.js
+++ models/User.model.js
@@ -1,13 +1,20 @@
 const { Schema, model } = require("mongoose");
 
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
-const userSchema = new Schema({
-  username: {
-    type: String,
-    // unique: true -> Ideally, should be unique, but its up to you
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+    },
+    password: String,
+    firstName: String,
+    lastName: String,
   },
-  password: String,
-});
+  {
+    timestamps: true,
+  }
+);
 
 const User = model("User", userSchema);
 
