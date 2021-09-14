6cbf841b4aa964e28c78e0e26cbc328b9401e178
kpla001 <kpla001@fiu.edu>
2021-09-13 20:52:42 -0400

    create Artist model

--- models/Track.model.js
+++ models/Track.model.js
@@ -3,15 +3,16 @@ const { Schema, model } = require("mongoose");
 const trackSchema = new Schema(
     {
         name: String,
-        SpotifyId: String,
+        spotifyId: String,
         artists: [{ type: Schema.Types.ObjectId, ref: "Artist" }],
         album: Schema.Types.ObjectId, ref: "Album",
         availableMarkets: [ String ],
         previewUrl: String,
         durationMS: Number,
+        popularity: Number,
     }
 )
 
-const Track = model("Track", trackSchema)
+const Track = model("Track", trackSchema);
 
 module.exports = Track;
